b355b2409349b93b8de6236d77228cad35057e6d
Dallin Guisti <dallin.guisti@gmail.com>
2020-12-24 22:20:16 -0800

    add installer

---
+++ Installer.py
@@ -0,0 +1,34 @@
+import os
+os.system("python -m pip install wget")
+import wget
+print("Downloading dependencies...")
+try:
+    wget.download("https://raw.githubusercontent.com/dguis/TextAdventure/master/Requirements.txt",bar=None)
+except:
+    print("Error. Failed to resolve dependency file. Please try again later.")
+    quit()
+os.system("python -m pip install -r requirements.txt")
+import wget
+import urllib.request
+
+def checkConnection(self):
+    try:
+        urllib.request.urlopen("https://github.com") #Python 3.x
+        return True
+    except:
+        return False
+
+if not checkConnection():
+    print("Error. Internet connection failed. Please try again later.")
+    quit()
+dir = os.getcwd()
+if os.path.exists(dir+"/Update.py"):
+    os.remove(dir+"/Update.py")
+print("Downloading update engine...")
+try:
+    wget.download("https://raw.githubusercontent.com/dguis/TextAdventure/master/Update.py",bar=None)
+except:
+    print("Error downloading upload engine. PLease try again later.")
+    quit()
+print("Update engine downloaded succesfully. Update engine starting...")
+os.system(f"python {dir}/Update.py")
